66fae4901e7a956242463e0de44513662a1cb55c
Salvador Guzman <guzmansalv@gmail.com>
2017-11-03 21:59:47 -0700

    pretty print array

--- src/test/scala/com/github/sguzman/hackerrank/corecs/algorithms/warmup/_1solvemefirst/SolutionTest.scala
+++ src/test/scala/com/github/sguzman/hackerrank/corecs/algorithms/warmup/_1solvemefirst/SolutionTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.FunSuite
 class SolutionTest extends FunSuite {
   val input = Array("2", "3")
   val expected = 5
-  test(s"Program should take ${input.toString} and return $expected") {
+  test(s"Program should take ${input.mkString("[", ", ", "]")} and return $expected") {
     assert(2 == 1 + 1)
   }
 
