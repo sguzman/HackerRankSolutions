50c88a9e42d6263e3c0175ae0db88e56b98ee358
Salvador Guzman <guzmansalv@gmail.com>
2017-11-03 21:55:51 -0700

    tested and ran into problem

--- src/main/scala/com/github/sguzman/hackerrank/corecs/algorithms/warmup/_1solvemefirst/Solution.scala
+++ src/main/scala/com/github/sguzman/hackerrank/corecs/algorithms/warmup/_1solvemefirst/Solution.scala
@@ -1,5 +1,11 @@
 package com.github.sguzman.hackerrank.corecs.algorithms.warmup._1solvemefirst
 
 object Solution {
+  def main(args: Array[String]) {
+    println(this.summation(io.Source.stdin.getLines))
+  }
 
+  def summation(varArgs: Iterator[String]): Int = {
+    varArgs.map(_.toInt).sum
+  }
 }
---
+++ src/test/scala/com/github/sguzman/hackerrank/corecs/algorithms/warmup/_1solvemefirst/SolutionTest.scala
@@ -0,0 +1,12 @@
+package com.github.sguzman.hackerrank.corecs.algorithms.warmup._1solvemefirst
+
+import org.scalatest.FunSuite
+
+class SolutionTest extends FunSuite {
+  val input = Array("2", "3")
+  val expected = 5
+  test(s"Program should take ${input.toString} and return $expected") {
+    assert(2 == 1 + 1)
+  }
+
+}
